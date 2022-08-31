98c24f0580174143f3ae2cbeb4f5537b83577b7c
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-31 16:06:18 +0930

    Update file structure for containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
