9eff44d6670be69472b84237a1f85ea4c007f612
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-31 18:11:20 +0930

    add images and minor fix

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,6 +1,40 @@
-import logo from '../components/assets/logo.png'
+import mobile from '../assets/mobile.png'
+import api from '../assets/api.png'
+import css from '../assets/css.png'
+import git from '../assets/git.png'
+import html from '../assets/html.png'
+import javascript from '../assets/javascript.png'
+import node from '../assets/node.png'
+import react from '../assets/react.png'
+import redux from '../assets/redux.png'
+import sass from '../assets/sass.png'
+
+import about01 from '../assets/about01.png'
+import about02 from '../assets/about02.png'
+import about03 from '../assets/about03.png'
+import about04 from '../assets/about04.png'
+
+import profile from '../assets/profile.png'
+import circle from '../assets/circle.svg'
+import logo from '../assets/logo.png'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  mobile,
+  api,
+  css,
+  git,
+  html,
+  javascript,
+  node,
+  react,
+  redux,
+  sass,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
   logo,
 }
