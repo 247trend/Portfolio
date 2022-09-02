4cf6c40a340ba24e289cd829c862ad5918c6720d
Jason Yuan <jasonyuan0130@gmail.com>
2022-09-02 22:07:37 +0930

    Add social links

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,13 +1,12 @@
-import { BsTwitter, BsGithub } from "react-icons/bs"
+import { BsGithub } from 'react-icons/bs'
 
 const SocialMedia = () => {
   return (
-    <div className="app__social">
+    <div className='app__social'>
       <div>
-        <BsTwitter />
-      </div>
-      <div>
-        <BsGithub />
+        <a href='https://github.com/247trend' target='_blank' rel='noreferrer'>
+          <BsGithub />
+        </a>
       </div>
     </div>
   )
